3e1037d538e4b97a71eaef38db03403e8ec3a9c2
Rishabh Kumar Mandal <rishabh@aicentre.org>
2025-08-22 12:22:38 +0530

    commit

---
+++ Python/variables/datatypes.py
@@ -0,0 +1,5 @@
+a= 12 # a is an integer
+b= 5.5 # b is a float
+c = "rishu" #c is a string
+d  = True # d is a boolean
+print (a)
---
+++ Python/variables/variables.py
@@ -0,0 +1,7 @@
+a  = 1
+b= 2
+print (a+b) 
+
+a= 30
+name = " rishu"
+age = 20
