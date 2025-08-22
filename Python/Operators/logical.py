4140fe4f2da89e62e0608085fcea12db12fa9155
Rishabh Kumar Mandal <rishabh@aicentre.org>
2025-08-22 12:48:56 +0530

    logical.py update

--- Python/Operators/logical.py
+++ Python/Operators/logical.py
@@ -1,6 +1,16 @@
+#Truth table of OR 
+
 a = True or False 
 print (a)
 print ("True of False is", True or False)
 print ("True of True is", True or False)
 print ("Flase of True is", True or False)
 print ("False of False is", True or False)
+
+
+#Truth table of AND
+print ("True and False is", True and False)
+print ("True and True is", True and False)
+print ("Flase and True is", True and False)
+print ("False and alse is", True and False)
+
