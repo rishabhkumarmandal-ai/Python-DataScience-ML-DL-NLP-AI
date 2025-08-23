f2c8ca51767c1d66d3f1ee84c8ad8407892ce585
Rishabh Kumar Mandal <rishabh@aicentre.org>
2025-08-23 13:56:08 +0530

    learn how to add

---
+++ Python/problems/problem3.py
@@ -0,0 +1,11 @@
+a = input("Enter your first number:")
+b = input("Enter your second number:")
+c= a + b
+print (c) #concatination
+
+
+
+d = int (input("enter your first number:"))
+e = int (input("enter your second number:"))
+f = d + e
+print (f)  #add without concatinate
--- Python/typecasting/type.py
+++ Python/typecasting/type.py
@@ -1,3 +1,4 @@
-a = " rishabh"
-t = type(a) 
+a = " 67.5"  #string
+b = float(a) #typecasting
+t = type(b)  #type function used to change the datatype
 print (t)
