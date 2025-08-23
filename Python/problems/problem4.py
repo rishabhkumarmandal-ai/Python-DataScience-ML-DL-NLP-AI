0b2e1549c4ca4a3bb2551bdf3db5544ad81218c2
Rishabh Kumar Mandal <rishabh@aicentre.org>
2025-08-23 14:13:14 +0530

    modulus operator

---
+++ Python/problems/problem4.py
@@ -0,0 +1,6 @@
+a = 39
+b= 6
+print(" Reminder when a is divided by b is: ", a%b) #modulus operator
+print(" floor division of a and b is: ", a//b) #floor division operator 
+print(" 2 raised to the power 3 is:" , 2**3) #exponentiation operator
+print (" true and true is ", True and True ) 
