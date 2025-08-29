3c460ab905f5705c3cd5ba81150bbed62d4506c0
Rishabh Kumar Mandal <rishabh@aicentre.org>
2025-08-29 20:23:56 +0530

    push

--- Python/Strings/concatenate.py
+++ Python/Strings/concatenate.py
@@ -2,8 +2,3 @@
 b = "Hello, World!"
 print(b[-5:-2])
 
-
-
-
-b = "Hello, World!"
-print(b[-7:-9])
--- Python/variables/datatypes.py
+++ Python/variables/datatypes.py
@@ -2,4 +2,4 @@ a= 12 # a is an integer
 b= 5.5 # b is a float
 c = "rishu" #c is a string
 d  = True # d is a boolean
-print (a)
+x= 1j # x is a complex number
