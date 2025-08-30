2eb5f2c20f3061e7d0e42a5b227428c274b0c301
Rishabh Kumar Mandal <rishabh@aicentre.org>
2025-08-30 13:41:32 +0530

    learn new things again

--- Python/Strings/slice.py
+++ Python/Strings/slice.py
@@ -6,3 +6,10 @@ print(b[2:5])
 b = "Hello, World!"
 print(b[-5:-2])
 
+# new
+name = "rishabh"
+nameshort = name[0:3]
+print (nameshort) 
+charchter1 = name[1] # print only the indexing one 
+print (charchter1)
+
